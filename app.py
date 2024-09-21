cf208d32563aa26da0a2ccf14325cd16bf029f5f
Noor Saeed <124183389+611noorsaeed@users.noreply.github.com>
2024-09-21 07:05:37 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 from fpdf import FPDF  # pip install fpdf
 
 # Set your API key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCDKFxhW56bIfs7ORISzrkB5NGzv_oVl9c"
+os.environ["GOOGLE_API_KEY"] = "your api here"
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 model = genai.GenerativeModel("models/gemini-1.5-pro")
 
